986cb6aa25e474652497c62df2e6089470390e25
Richard <richard_875@me.com>
2022-06-11 14:47:08 +1000

    Initial Commit

---
+++ MailyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 6/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MailyApp/MailyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailyAppApp.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 6/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MailyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
