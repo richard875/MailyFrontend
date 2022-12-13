1c805434b2d3a8c36e0bf3ed4ece3a4e4afc710d
Richard Lee <richard_875@me.com>
2022-12-13 21:41:32 +1100

    - Add mailyApp shared file

---
+++ MailyApp/SharedUserDefaults.swift
@@ -0,0 +1,16 @@
+//
+//  SharedUserDefaults.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 6/12/2022.
+//
+
+import Foundation
+
+struct SharedUserDefaults {
+    static let suiteName = "group.com.TeamMaily.MailyApp"
+    
+    struct Keys {
+        static let username = "username"
+    }
+}
