cb27b73c6f97d40d1b41b2ba7165f9a436bcbb72
Richard Lee <richard_875@me.com>
2022-12-13 21:43:09 +1100

    - Add Maily Extension shared file

---
+++ Maily/MailExtension.swift
@@ -0,0 +1,25 @@
+//
+//  MailExtension.swift
+//  Maily
+//
+//  Created by Richard Lee on 6/11/2022.
+//
+
+import MailKit
+
+class MailExtension: NSObject, MEExtension {
+    
+    func handlerForMessageActions() -> MEMessageActionHandler {
+        // Use a shared instance for all messages, since there's
+        // no state associated with performing actions.
+        return MessageActionHandler.shared
+    }
+
+    func handler(for session: MEComposeSession) -> MEComposeSessionHandler {
+        // Create a unique instance, since each compose window is separate.
+        return ComposeSessionHandler()
+    }
+
+    
+}
+
