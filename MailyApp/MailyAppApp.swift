62bc98eaf6f616f5452ae32cbbfdddc017289886
Richard Lee <richard_875@me.com>
2022-12-13 21:42:30 +1100

    - Move mailyApp to menu bar

--- MailyApp/MailyAppApp.swift
+++ MailyApp/MailyAppApp.swift
@@ -9,9 +9,40 @@ import SwiftUI
 
 @main
 struct MailyAppApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "chart.line.uptrend.xyaxis.circle", accessibilityDescription: "Chart Line")
+            statusButton.action = #selector(togglePopover)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 300, height: 300)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: ContentView())
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
         }
     }
 }
