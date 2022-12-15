b1eb94c80f754682e32fc6b4fd8fd5adf3505d38
Richard Lee <richard_875@me.com>
2022-12-15 16:30:46 +1100

    - Remove message action logic

--- Maily/Action/MessageActionHandler.swift
+++ Maily/Action/MessageActionHandler.swift
@@ -13,16 +13,16 @@ class MessageActionHandler: NSObject, MEMessageActionHandler {
     
     func decideAction(for message: MEMessage, completionHandler: @escaping (MEMessageActionDecision?) -> Void) {
         // The action to take on the message, if any.
-        var action: MEMessageActionDecision? = nil
+        // var action: MEMessageActionDecision? = nil
         
         // Check if the subject of the message contains the word Mars.
         // If it does, specify an action to set the background color to red.
-        if message.subject.contains("Mars") {
-            action = MEMessageActionDecision.action(.setBackgroundColor(.red))
-        }
+        // if message.subject.contains("Mars") {
+            // action = MEMessageActionDecision.action(.setBackgroundColor(.red))
+        // }
         
         // Always call the completion handler, passing the action
         // to take, or nil if there's no action.
-        completionHandler(action)
+        // completionHandler(action)
     }
 }
