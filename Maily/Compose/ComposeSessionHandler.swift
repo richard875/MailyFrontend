bcb9173619f27844dd1199c85a6cccbd954a260e
Richard Lee <richard_875@me.com>
2022-12-15 19:56:38 +1100

    - Remove unneeded code
    - Command functions

--- Maily/Compose/ComposeSessionHandler.swift
+++ Maily/Compose/ComposeSessionHandler.swift
@@ -8,7 +8,7 @@
 import MailKit
 
 class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
-    
+    // MARK: - Start composing an email
     func mailComposeSessionDidBegin(_ session: MEComposeSession) {
         // Perform any setup necessary for handling the compose session.
         let sharedUserDefaults = UserDefaults(suiteName: SharedUserDefaults.suiteName)
@@ -21,33 +21,13 @@ class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
         print(separator)
     }
     
+    // MARK: - Finish composing an email
     func mailComposeSessionDidEnd(_ session: MEComposeSession) {
         // Perform any cleanup now that the compose session is over.
-    }
-    
-    // MARK: - Annotating Address Tokens
-    
-    func annotateAddressesForSession(_ session: MEComposeSession) async -> [MEEmailAddress: MEAddressAnnotation] {
-        var annotations: [MEEmailAddress: MEAddressAnnotation] = [:]
+        session.reload()
         
-        // Iterate through all the receipients in the message.
-        //
-        // NOTE: If there isn't any definitive information we can provide about
-        // the address, don't add an annotation for it.
-        for address in session.mailMessage.allRecipientAddresses {
-            // If the address ends in @example.com, indicate that's an error.
-            if address.rawString.hasSuffix("@example.com") {
-                // Create an address annotation that Mail shows in the address token.
-                let message = "example.com is not a valid domain"
-                let annotation = MEAddressAnnotation.error(withLocalizedDescription: message)
-                
-                // Add the annotation to the results dictionary.
-                annotations[address] = annotation
-            }
-        }
-        
-        // Call the Completion handler with the results.
-        return annotations
+        // var clickedSend: true if message header contains the 'subject' key
+        let clickedSend = session.mailMessage.headers?["subject"] != nil
     }
     
     // MARK: - Displaying Custom Compose Options
@@ -56,27 +36,7 @@ class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
         return ComposeSessionViewController(nibName: "ComposeSessionViewController", bundle: Bundle.main)
     }
     
-    // MARK: - Adding Custom Headers
-    
-    func additionalHeaders(for session: MEComposeSession) -> [String : [String]] {
-        // To insert custom headers into a message, return a dictionary with
-        // the key and an array of one or more values.
-        return ["X-CustomHeader": ["This ia a custom header."]]
-    }
-    
     // MARK: - Confirming Message Delivery
-    
-    enum ComposeSessionError: LocalizedError {
-        case invalidRecipientDomain
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidRecipientDomain:
-                return "example.com is not a valid recipient domain"
-            }
-        }
-    }
-    
     func allowMessageSendForSession(_ session: MEComposeSession, completion: @escaping (Error?) -> Void) {
         // Before Mail sends a message, your extension can validate the
         // contents of the compose session. If the message is ready to be sent,
