4f8a9d8eb349c230048650ae3f236b41588ec9cd
Richard Lee <richard_875@me.com>
2022-12-16 22:27:56 +1100

    - Seperate Login view file

--- MailyApp/ContentView.swift
+++ MailyApp/ContentView.swift
@@ -13,38 +13,8 @@ struct ContentView: View {
     let localS = UserDefaults.standard
     
     // Actual code
-    @State private var email: String = ""
-    @State private var password: String = ""
-    
     var body: some View {
-        VStack {
-            Text("Welcome to Maily")
-                .font(.system(size: 25, weight: .bold))
-            Text("Login to continue")
-            TextField("Email", text: $email)
-            SecureField("Password", text: $password)
-            
-            Button(action: {}) {
-                Text("Sign In")
-                    .padding(22)
-                    .frame(width: 222, height: 44)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-            }.frame(width: 222, height: 44)
-             .background(Color.blue).cornerRadius(10)
-            
-//            Button("Store local storage 2 (myUserName)") {
-//                sharedUserDefaults?.set("myUserName", forKey: SharedUserDefaults.Keys.username)
-//            }
-        }.frame(
-            minWidth: 0,
-            maxWidth: .infinity,
-            minHeight: 0,
-            maxHeight: .infinity,
-            alignment: .topLeading
-          )
-          .background(Color.white)
-        .frame(width: 300, height: 500)
+        Login()
     }
 }
 
@@ -53,3 +23,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Button("Store local storage 2 (myUserName)") {
+//   sharedUserDefaults?.set("myUserName", forKey: SharedUserDefaults.Keys.username)
+// }
