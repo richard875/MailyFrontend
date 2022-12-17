2213cd96b1671f8661ab6ad5fbb4129fa5d7398a
Richard Lee <richard_875@me.com>
2022-12-18 04:57:55 +1100

    - Add LoginResponse structure

---
+++ MailyApp/src/Models/LoginResponse.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResponse.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 12/18/22.
+//
+
+import Foundation
+
+struct LoginResponse {
+    let status: ReturnStatus
+    let message: String
+}
