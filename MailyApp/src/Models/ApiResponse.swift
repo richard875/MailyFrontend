f52154936c72b95e4857704aab534c820f2a136d
Richard Lee <richard_875@me.com>
2022-12-18 17:29:26 +1100

    - Add ApiResponse structure

---
+++ MailyApp/src/Models/ApiResponse.swift
@@ -0,0 +1,13 @@
+//
+//  ApiResponse.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 12/18/22.
+//
+
+import Foundation
+
+struct ApiResponse {
+    let data: Any
+    let status: HTTPResponseStatus
+}
