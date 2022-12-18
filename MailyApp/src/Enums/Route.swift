b2d086a94548eafa0be60c5a9ac8e5bdf561b41f
Richard Lee <richard_875@me.com>
2022-12-18 22:21:59 +1100

    - Add Route enum

---
+++ MailyApp/src/Enums/Route.swift
@@ -0,0 +1,16 @@
+//
+//  Route.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 12/18/22.
+//
+
+import Foundation
+
+enum Route: String {
+    case LOGIN
+    case INDEX
+    case FORGOT_PASSWORD
+    case SIGN_UP
+    
+}
