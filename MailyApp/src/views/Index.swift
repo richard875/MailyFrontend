bff94c84f9c3b1fe9ecb713c95883567e0cdc28c
Richard Lee <richard_875@me.com>
2022-12-18 14:34:58 +1100

    - Add Index page

---
+++ MailyApp/src/views/Index.swift
@@ -0,0 +1,21 @@
+//
+//  Index.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 12/18/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Index: View {
+    var setLoggedIn: ((Bool) -> Void)
+    
+    var body: some View {
+        VStack(spacing: 0) {
+        }
+        .frame(width: 300, height: 439)
+        .background(Color("Background"))
+    }
+}
+
