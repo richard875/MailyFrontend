777c9b946ac05cedfd122d808e362fb96b0a6a5b
Richard Lee <richard_875@me.com>
2022-12-18 18:02:39 +1100

    - Modify UserLogin return type

--- MailyApp/src/Methods/UserLogin.swift
+++ MailyApp/src/Methods/UserLogin.swift
@@ -12,26 +12,27 @@ internal func login(email: String, password: String, completion: @escaping (Logi
         apiMethod: HTTPMethod.POST,
         apiEndpoint: "\(ApiEndpoints.ServerUrl)/\(ApiEndpoints.Login)",
         apiBody: ["email": email, "password": password],
-        completion: {(result, error) in completion(loginComplete(result: result, error: error))}
+        completion: {(result, error) in completion(loginComplete(result: (result as! ApiResponse), error: error))}
     )
 }
 
-func loginComplete(result: Optional<Any>, error: Optional<Error>) -> LoginResponse {
-//    let defaults = UserDefaults.standard
-    let apiResult = error != nil ? error : result // Assign error if it exists, otherwise assign result
+func loginComplete(result: ApiResponse, error: Optional<Error>) -> LoginResponse {
+    if (error != nil) {
+        return LoginResponse(returnStatus: ReturnStatus.ERROR, httpStatus: result.status, message: error!.localizedDescription)
+    }
     
     // Check if the "status" property is in the result
-    if let apiResultDict = apiResult as? [String: Any], apiResultDict["status"] != nil {
-        return LoginResponse(status: ReturnStatus.ERROR, message: "Username or Password is incorrect")
+    if let apiResultDict = result.data as? [String: Any], apiResultDict["status"] != nil {
+        return LoginResponse(returnStatus: ReturnStatus.ERROR, httpStatus: result.status, message: "Username or Password is incorrect")
     }
     
     // If the "status" property is not in the result, then it is a success
-    // Try to cast the result to a dictionary
-    guard let apiResultDict = apiResult as? [String: Any] else {
-        return LoginResponse(status: ReturnStatus.ERROR, message: "Unknown error") // Result cannot be cast to a dictionary
+    // Try to cast the result to a dictionary, throw error if result cannot be cast to a dictionary
+    guard let apiResultDict = result.data as? [String: Any] else {
+        return LoginResponse(returnStatus: ReturnStatus.ERROR, httpStatus: result.status, message: "Unknown error")
     }
     
     // Get the value of the "token" variable
     let token = apiResultDict["token"] as? String
-    return LoginResponse(status: ReturnStatus.SUCCESS, message: token!)
+    return LoginResponse(returnStatus: ReturnStatus.SUCCESS, httpStatus: result.status, message: token!)
 }
