c84443a2f93b9d3e0a810d710342d080584c7e15
Richard Lee <richard_875@me.com>
2022-12-20 13:28:53 +1100

    - Modify apiMethod function
    - Add auth and token, and make apiBody Optional

--- MailyApp/src/Api/ApiMethod.swift
+++ MailyApp/src/Api/ApiMethod.swift
@@ -7,52 +7,53 @@
 
 import Foundation
 
-func apiMethod(apiMethod: HTTPMethod, apiEndpoint: String, apiBody: Any, completion: @escaping (Any?, Error?) -> Void) {
-    // Set up the URL request
-    guard let url = URL(string: apiEndpoint) else {
-        completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
-        return
-    }
-    var urlRequest = URLRequest(url: url)
-    urlRequest.httpMethod = apiMethod.rawValue
-    
-    // Set up the HTTP body
-    let jsonBody: Data
-    do {
-        jsonBody = try JSONSerialization.data(withJSONObject: apiBody, options: [])
-        urlRequest.httpBody = jsonBody
-    } catch {
-        completion(nil, NSError(domain: "Invalid JSON body", code: 0, userInfo: nil))
-        return
-    }
-    
-    // Set up the HTTP header
-    urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    
-    // Make the request
-    let session = URLSession.shared
-    let task = session.dataTask(with: urlRequest) { (data, response, error) in
-        // Check for any errors
-        if let error = error {
-            completion(nil, error)
+func apiMethod(
+    apiMethod: HTTPMethod,
+    apiEndpoint: String,
+    apiBody: Any? = nil, // Optional
+    auth: Bool = false,  // Optional
+    token: String = "",  // Optional
+    completion: @escaping (Any?, Error?) -> Void) {
+        // Set up the URL request
+        guard let url = URL(string: apiEndpoint) else {
+            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
             return
         }
-        // Check that we received data
-        guard let responseData = data else {
-            completion(nil, NSError(domain: "No data received", code: 0, userInfo: nil))
-            return
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = apiMethod.rawValue
+        
+        if (auth) {
+            urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         }
         
-        // Return received data
-        do {
+        // Set up the HTTP body
+        if (apiBody != nil) {
+            do {
+                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: apiBody!, options: [])
+            } catch {
+                completion(nil, NSError(domain: "Invalid JSON body", code: 0, userInfo: nil))
+                return
+            }
+        }
+        
+        // Set up the HTTP header
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Make the request
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            // Check for any errors
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            // Return received data
             let httpResponse = response as! HTTPURLResponse
             
-            let jsonObject = try JSONSerialization.jsonObject(with: responseData, options: [])
+            let jsonObject = try? JSONSerialization.jsonObject(with: data!, options: []) // Try serialize data to JSON, return nil if failed
             let statusCode = httpResponse.statusCode
+            
             completion(ApiResponse(data: jsonObject, status: HTTPResponseStatus(rawValue: statusCode)!), nil)
-        } catch  {
-            completion(nil, NSError(domain: "Error parsing response", code: 0, userInfo: nil))
         }
+        task.resume()
     }
-    task.resume()
-}
