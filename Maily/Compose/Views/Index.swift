ea488b1446d7475b1218c3c921f3203797295ac0
Richard Lee <richard_875@me.com>
2022-12-22 01:06:37 +1100

    - Complete Compose view

--- Maily/Compose/Views/Index.swift
+++ Maily/Compose/Views/Index.swift
@@ -11,14 +11,33 @@ import MailKit
 
 struct ComposeView: View {
     let session: MEComposeSession
+    @State var html: String
     
     var body: some View {
-        VStack(spacing: 0) {
-            Text("Test")
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Tracker Code")
+                .font(.system(size: 23))
+                .fontWeight(.semibold)
+            Text("Paste the HTML into Email")
+                .padding(.top, 5)
+                .font(.system(size: 13))
+            Divider()
+                .frame(width: 260)
+                .overlay(Color("Divider"))
+                .padding(.top, 12)
+            WebView(html: $html)
+                .frame(width: 260, height: 115)
+                .border(Color("Divider"), width: 1)
+                .cornerRadius(3)
+                .padding(.top, 15)
         }
-        .frame(width: 300, height: 439)
+        .padding(.top, 20)
+        .padding(.leading, 20)
+        .frame(
+            width: 300,
+            height: 230,
+            alignment: .topLeading
+        )
         .background(Color("Background"))
     }
 }
-
-
