fc138c7252545bd1802111ddd2b28257aa9a0349
Richard Lee <richard_875@me.com>
2022-12-24 21:14:06 +1100

    - Add serverAddress and trackingNumber to compose view

--- Maily/Compose/ComposeSessionViewController.swift
+++ Maily/Compose/ComposeSessionViewController.swift
@@ -9,10 +9,14 @@ import MailKit
 import SwiftUI
 
 class ComposeSessionViewController: MEExtensionViewController {
-    let session: MEComposeSession
+    private let session: MEComposeSession
+    private var serverAddress: String?
+    private var trackingNumber: String?
     
-    init(session: MEComposeSession) {
+    init(session: MEComposeSession, serverAddress: String?, trackingNumber: String?) {
         self.session = session
+        self.serverAddress = serverAddress
+        self.trackingNumber = trackingNumber
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -21,11 +25,12 @@ class ComposeSessionViewController: MEExtensionViewController {
     }
     
     override func loadView() {
-        // Load the HTML page into the web view.
+        // Load the HTML page into the web view and replace witht the tracking URL
         let htmlPath = Bundle.main.path(forResource: "tracking", ofType: "html")
         let url = URL(fileURLWithPath: htmlPath!)
-        let html = try? String(contentsOf: url)
-                
+        let html = try? String(contentsOf: url).replacingOccurrences(of: "{{beacon}}", with: "\(self.serverAddress!)\(self.trackingNumber!)")
+        
+        // Load view
         let hostingController = NSHostingController(rootView: ComposeView(session: session, html: html!))
         self.view = hostingController.view
     }
