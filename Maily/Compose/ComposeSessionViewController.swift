d25bfdffc3d53d42128551fb48bd5dfc09038dda
Richard Lee <richard_875@me.com>
2022-12-27 17:29:23 +1100

    - Add png suffix to picture

--- Maily/Compose/ComposeSessionViewController.swift
+++ Maily/Compose/ComposeSessionViewController.swift
@@ -28,7 +28,7 @@ class ComposeSessionViewController: MEExtensionViewController {
         // Load the HTML page into the web view and replace witht the tracking URL
         let htmlPath = Bundle.main.path(forResource: "tracking", ofType: "html")
         let url = URL(fileURLWithPath: htmlPath!)
-        let html = try? String(contentsOf: url).replacingOccurrences(of: "{{beacon}}", with: "\(self.serverAddress!)\(self.trackingNumber!)")
+        let html = try? String(contentsOf: url).replacingOccurrences(of: "{{beacon}}", with: "\(self.serverAddress!)\(self.trackingNumber!).png")
         
         // Load view
         let hostingController = NSHostingController(rootView: ComposeView(session: session, html: html!))
