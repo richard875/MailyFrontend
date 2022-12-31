8b161bc1e63c2c2c7948a41fe0ce317e80dfccf0
Richard Lee <richard_875@me.com>
2022-12-31 19:04:43 +1100

    - Add AssignTrackingResponse model

---
+++ Maily/src/Models/AssignTrackingResponse.swift
@@ -0,0 +1,14 @@
+//
+//  AssignTrackingResponse.swift
+//  Maily
+//
+//  Created by Richard Lee on 12/31/22.
+//
+
+import Foundation
+
+struct AssignTrackingResponse {
+    let returnStatus: ReturnStatus
+    let httpStatus: HTTPResponseStatus
+    let data: Any?
+}
