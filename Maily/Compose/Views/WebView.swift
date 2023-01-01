bb2f2fb5aa4cbfcbe06ee42440af83e1a394bd94
Richard Lee <richard_875@me.com>
2023-01-01 19:51:52 +1100

    - Add a rule list to block all outgoing connections for the WebView component

--- Maily/Compose/Views/WebView.swift
+++ Maily/Compose/Views/WebView.swift
@@ -13,8 +13,41 @@ struct WebView: NSViewRepresentable {
     @Binding var html: String
     
     func makeNSView(context: Context) -> WKWebView {
-        return WKWebView()
+        // Define a content rule list that blocks all outgoing connections
+        let blockAllRule = """
+            [{
+                "trigger": {
+                    "url-filter": ".*"
+                },
+                "action": {
+                    "type": "block"
+                }
+            }]
+        """
+        
+        // Convert the rule list string to data and then back to a string
+        let blockAllRuleData = blockAllRule.data(using: .utf8)!
+        let blockAllRuleString = String(data: blockAllRuleData, encoding: .utf8)!
+        
+        // Create a WKWebView
+        let webView = WKWebView()
+        
+        // Create a WKContentRuleListStore and compile the rule list
+        if let ruleListStore = WKContentRuleListStore.default() {
+            ruleListStore.compileContentRuleList(forIdentifier: "BlockAllRuleList", encodedContentRuleList: blockAllRuleString) { (ruleList, error) in
+                if let error = error {
+                    print("Error compiling content rule list: \(error)")
+                } else if let ruleList = ruleList {
+                    // Add the compiled rule list to the user content controller
+                    webView.configuration.userContentController.add(ruleList)
+                }
+            }
+        }
+        
+        return webView
     }
+    
+    
     
     func updateNSView(_ nsView: WKWebView, context: Context) {
         nsView.loadHTMLString(html, baseURL: nil)
