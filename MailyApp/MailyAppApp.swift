62c03631fe20552b450283ded2c6773dbfa109d0
Richard Lee <richard_875@me.com>
2023-01-07 23:52:51 +1100

    - Add secondaryPopover

--- MailyApp/MailyAppApp.swift
+++ MailyApp/MailyAppApp.swift
@@ -21,6 +21,7 @@ struct MailyAppApp: App {
 class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
     private var statusItem: NSStatusItem!
     private var mainPopover: NSPopover!
+    private var secondaryPopover: NSPopover!
     
     @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.windows.first { window.close() }
@@ -37,6 +38,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
         mainPopover.contentViewController = NSHostingController(rootView: ContentView())
         mainPopover.setValue(true, forKeyPath: "shouldHideAnchor")
         
+        // Create and show the second content view window as a popup
+        secondaryPopover = NSPopover()
+        secondaryPopover.contentSize = NSSize(width: 350, height: 500)
+        secondaryPopover.behavior = .transient
+        secondaryPopover.contentViewController = NSHostingController(rootView: DetailedContentView())
+        secondaryPopover.setValue(true, forKeyPath: "shouldHideAnchor")
     }
     
     @objc func togglePopover() {
