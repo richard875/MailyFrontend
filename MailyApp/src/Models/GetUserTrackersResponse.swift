cb43e073b39b2095b6b8c596ba42b12ce540477b
Richard Lee <richard_875@me.com>
2023-01-09 00:10:41 +1100

    - Add GetUserTrackersResponse struct

---
+++ MailyApp/src/Models/GetUserTrackersResponse.swift
@@ -0,0 +1,14 @@
+//
+//  GetUserTrackersResponse.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 1/8/23.
+//
+
+import Foundation
+
+struct GetUserTrackersResponse {
+    let returnStatus: ReturnStatus
+    let httpStatus: HTTPResponseStatus
+    let user: [Tracker]?
+}
