74b76a137e20aa6b95d6dedb877fbb7bf93e2e71
Richard Lee <richard_875@me.com>
2023-01-09 23:36:27 +1100

    - Add SearchUserTrackers method

---
+++ MailyApp/src/Methods/SearchUserTrackers.swift
@@ -0,0 +1,81 @@
+//
+//  SearchUserTrackers.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 1/9/23.
+//
+
+import Foundation
+
+internal func SearchUserTrackers(token: String, searchQuery: String, completion: @escaping (GetUserTrackersResponse) -> Void) {
+    apiMethod(
+        apiMethod: HTTPMethod.GET,
+        apiEndpoint: "\(ApiEndpoints.ServerUrl)/\(ApiEndpoints.SearchTrackers)/\(searchQuery)",
+        auth: true,
+        token: token,
+        completion: {(result, error) in completion(completeSearch(result: (result as! ApiResponse), error: error))}
+    )
+}
+
+func completeSearch(result: ApiResponse, error: Optional<Error>) -> GetUserTrackersResponse {
+    // First check if there's errors. Return straight away if there are any
+    if (error != nil) {
+        return GetUserTrackersResponse(
+            returnStatus: ReturnStatus.ERROR,
+            httpStatus: result.status,
+            userTrackers: nil
+        )
+    }
+    
+    // Continue if no error
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSxxxxx"
+    
+    guard let trackerArray = result.data as? [[String: Any]] else {
+        return GetUserTrackersResponse(
+            returnStatus: ReturnStatus.ERROR,
+            httpStatus: result.status,
+            userTrackers: []
+        )
+    }
+    
+    let trackers = trackerArray.compactMap {(tracker) -> Tracker in
+        let id = tracker["id"] as! String
+        let createdAt = dateFormatter.date(from: tracker["CreatedAt"] as? String ?? "")
+        let updatedAt = dateFormatter.date(from: tracker["UpdatedAt"] as? String ?? "")
+        let deletedAt = dateFormatter.date(from: tracker["DeletedAt"] as? String ?? "")
+        let userID = tracker["userId"] as! String
+        let timesOpened = tracker["timesOpened"] as! Int
+        let composeAction = tracker["composeAction"] as! Int
+        let subject = tracker["subject"] as? String
+        let fromAddress = tracker["fromAddress"] as! String
+        let toAddresses = tracker["toAddresses"] as! String
+        let ccAddresses = tracker["ccAddresses"] as? String
+        let bccAddresses = tracker["bccAddresses"] as? String
+        let replyToAddresses = tracker["replyToAddresses"] as? String
+        let internalMessageID = tracker["internalMessageID"] as? String
+        
+        return Tracker(
+            id: id,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            deletedAt: deletedAt,
+            userID: userID,
+            timesOpened: timesOpened,
+            composeAction: composeAction,
+            subject: subject,
+            fromAddress: fromAddress,
+            toAddresses: toAddresses,
+            ccAddresses: ccAddresses,
+            bccAddresses: bccAddresses,
+            replyToAddresses: replyToAddresses,
+            internalMessageId: internalMessageID
+        )
+    }
+    
+    return GetUserTrackersResponse(
+        returnStatus: ReturnStatus.SUCCESS,
+        httpStatus: result.status,
+        userTrackers: trackers
+    )
+}
