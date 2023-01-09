672c5b2ef21b304979f1dac8336e71dce088f4d2
Richard Lee <richard_875@me.com>
2023-01-09 19:06:55 +1100

    - Add IndexEmail enum

---
+++ MailyApp/src/Enums/IndexEmail.swift
@@ -0,0 +1,14 @@
+//
+//  IndexEmail.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 1/9/23.
+//
+
+import Foundation
+
+enum IndexEmail: String {
+    case ALL = "All"
+    case OPENED = "Opened"
+    case UNOPENED = "Unopened"
+}
