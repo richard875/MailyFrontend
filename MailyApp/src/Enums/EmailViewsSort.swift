5f6556fd36aac91fe854cb8d84c74f4bf1cb239e
Richard Lee <richard_875@me.com>
2023-01-11 19:33:22 +1100

    - Add EmailViewsSort enum

---
+++ MailyApp/src/Enums/EmailViewsSort.swift
@@ -0,0 +1,13 @@
+//
+//  EmailViewsSort.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 1/11/23.
+//
+
+import Foundation
+
+enum EmailViewsSort: String {
+    case LATEST_TO_OLDEST = "Latest to Oldest"
+    case OLDEST_TO_LATEST = "Oldest to Latest"
+}
