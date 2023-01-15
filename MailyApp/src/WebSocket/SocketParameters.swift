14596de9d379031fa11fb1e5a7534a4aab3630f6
Richard Lee <richard_875@me.com>
2023-01-16 09:41:13 +1100

    - Feature: Add SocketParameters file

---
+++ MailyApp/src/WebSocket/SocketParameters.swift
@@ -0,0 +1,12 @@
+//
+//  SocketParameters.swift
+//  MailyApp
+//
+//  Created by Richard Lee on 1/15/23.
+//
+
+import Foundation
+
+struct SocketParameters {
+    static let UpdateSignal = "update"
+}
